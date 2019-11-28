027faacd632cad652910f3e048ee6a902f8c9bc2
George Florea Bănuș <georgefb899@gmail.com>
2019-11-28 16:37:24 +0200

    - bump version to 2.0.4

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
-    QCoreApplication::setApplicationVersion("2.0.3");
+    QCoreApplication::setApplicationVersion("2.0.4");
     KLocalizedString::setApplicationDomain("quickaccess");
     QApplication::setQuitOnLastWindowClosed(false);
     MainWindow *w = new MainWindow();
