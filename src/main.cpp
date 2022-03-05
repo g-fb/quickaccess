3b1b7700644237f2526d3f5e180c31e6ea7204d1
George Florea Bănuș <georgefb899@gmail.com>
2022-03-05 06:00:23 +0200

    chore: version 3.0.0

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
-    QCoreApplication::setApplicationVersion("2.0.9");
+    QCoreApplication::setApplicationVersion("3.0.0");
     KLocalizedString::setApplicationDomain("quickaccess");
     QApplication::setQuitOnLastWindowClosed(false);
     MainWindow w;
